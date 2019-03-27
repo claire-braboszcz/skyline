eb313dbfeef740d5040b6892e9732cbbfc4eeaba
Claire Braboszcz <claire@guakamole.org>
2019-03-27 15:56:45 +0000

    created gender balance for each study part

--- Skyline-study-codes/skyline_random_id_cond.py
+++ Skyline-study-codes/skyline_random_id_cond.py
@@ -7,14 +7,15 @@ Created on Thu Jan 31 15:37:12 2019
 """
 
 import random   
-import numpy as np
 import pandas as pd
-import string
 
 
 # generate random study ID
 #def rand_id_gen(size, chars= string.digits):
 #    return ''.join(random.choice(chars) for x in range(size))
+
+
+savepath ='/home/claire/Documents/STUDY/EEG-Tobacco/Skyline Participants Forms/'
 
 #--------------------------
 # create random study ID
@@ -22,17 +23,30 @@ import string
     
 random.seed(a=5)
 
-study_id=[]
+study_id_all=[]
 #for i in range (1, 141):
-study_id = random.sample(range(100, 900), 140) # random list between 100 and 900 without duplicate
+study_id_all = random.sample(range(100, 800), 400) # random list between 100 and 900 without duplicate
 
 
+study_id_main_m = study_id_all[0:100]
+study_id_main_f = study_id_all[100:200]
 
-screening_id = list(range(1, 141))
+study_id_eeg_m = study_id_all[200:300]
+study_id_eeg_f = study_id_all[300:400]
+
+
+screening_id_main_m = ['m'+ str(id) for id in(range(1, 101))]
+screening_id_main_f = ['f'+ str(id) for id in(range(1, 101))]
+screening_id_eeg_m = ['eeg_m'+ str(id) for id in(range(1, 101))]
+screening_id_eeg_f = ['eeg_f'+ str(id) for id in(range(1, 101))]
 
 
 #create dictionary
-d = {'Screening ID' : screening_id, 'Study ID': study_id}
+d = {'Screening ID Main Male' : screening_id_main_m, 'Study ID Main Male': study_id_main_m, 
+     'Screening ID Main Female' : screening_id_main_f, 'Study ID Main Female': study_id_main_f, 
+     'Screening ID EEG Male' : screening_id_eeg_m, 'Study ID EEG Male': study_id_eeg_m, 
+     'Screening ID EEG Female' : screening_id_eeg_f, 'Study ID EEG Female': study_id_eeg_f
+     }
 
 #create dataframe
 df = pd.DataFrame(d)
@@ -42,8 +56,9 @@ df.to_csv('skyline_screening_study_id.csv')
 #----------------------------
 # Randomize study conditions
 #----------------------------
+
 # attribute condition
-nr_subjects = 75
+nr_subjects = 200
 batch_nr = 10
 total_cond = []
 for i in range(0, int(nr_subjects/batch_nr)):
@@ -52,7 +67,17 @@ for i in range(0, int(nr_subjects/batch_nr)):
     total_cond += nr
 
 
-d_cond ={'Study ID': study_id, 'Condition': total_cond }
+cond_main_m = total_cond[0:100]
+cond_main_f = total_cond[100:200]
+cond_eeg_m = total_cond[200:300]
+cond_eeg_f = total_cond[300:400]
+
+
+d_cond ={ 'Study ID Main Male': study_id_main_m, 'Cond Main Male': cond_main_m, 
+     'Study ID Main Female': study_id_main_f, 'Cond Main Female': cond_main_f, 
+    'Study ID EEG Male': study_id_eeg_m, 'Cond EEG Male': cond_eeg_m, 
+    'Study ID EEG Female': study_id_eeg_f,'Cond EEG Female': cond_eeg_f
+     }
 
 df_cond = pd.DataFrame(d_cond)
 
@@ -63,11 +88,19 @@ df_cond.to_csv('skyline_study_id_cond.csv')
 # Study ID + Phone numbers
 #-----------------------------------------------
 
-phone =[0]*len(study_id)
+#phone =[0]*len(study_id)
 
-d_phone = {'Study ID': study_id, 'Phone':phone}
+#d_phone = {'Study ID': study_id, 'Phone':phone}
 
 
-df_phone = pd.DataFrame(d_phone)
+#df_phone = pd.DataFrame(d_phone)
 
-df_phone.to_csv('skyline_study_id_phone_numbers.csv')
+#df_phone.to_csv('skyline_study_id_phone_numbers.csv')
+
+
+
+
+
+
+
+
