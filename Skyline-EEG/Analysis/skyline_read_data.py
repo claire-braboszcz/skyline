ded61e9c55e94d82ce27d96264887793d272fd0f
Claire Braboszcz <claire@guakamole.org>
2019-07-18 17:41:07 +0100

    started eeg data preprocess script

--- Skyline-EEG/Analysis/skyline_read_data.py
+++ Skyline-EEG/Analysis/skyline_read_data.py
@@ -11,8 +11,55 @@ import mne
 
 import numpy as np
 
-file_path = '/media/claire/Skyline EEG/Data/Pilot_04_10/pilot_10_04.vhdr'
+filename = '/home/claire/Documents/STUDY/EEG-Tobacco/DATA/s558/EEG/session_02/s558_02.vhdr'
 
-raw = mne.io.read_raw_brainvision(file_path, preload=True)
+raw = mne.io.read_raw_brainvision(filename, preload=True)
 
 events, _ = mne.events_from_annotations(raw)
+
+mne.write_events(op.join('/home/claire/Documents/STUDY/EEG-Tobacco/DATA/s558/EEG/session_02/', 's558_s02.eve'), events)
+
+montage= mne.channels.read_montage('standard_1020', path = '/home/claire/Applications/mne-python/mne/channels/data/montages/')# change to where you've put you mne python folder
+
+raw.set_montage(montage)
+
+raw.set_eeg_reference(ref_channels='average') # adjust to the kind of reference you want
+
+raw_fir = raw.filter(1.,40, fir_design='firwin')
+
+
+raw.plot()
+
+
+# epoching data
+
+#event_id = {'break' :50, 'start_go':55, 'start_pv':66, 'HW':21, 'NEG':25, 'NEUT':22, 'GO':11, 'NOGO':13}
+
+# only get go and nogo trials
+event_id = {'GO':11, 'NOGO':13}
+
+
+tmin = -0.2
+tmax = 0.5
+
+epochs = mne.Epochs(raw, events, event_id, tmin, tmax,
+                    baseline=(None, 0), reject=None,
+                    verbose=False, detrend=0,  preload=True)
+
+epochs.plot()
+
+
+# compute evoked data
+
+ep_go, ep_nogo = epochs['GO'].average(), epochs['NOGO'].average()
+
+
+mne.viz.plot_compare_evokeds([ep_go, ep_nogo], picks= 'eeg')
+
+
+diff_evoked = mne.combine_evoked([ep_go, -ep_nogo ], weights= 'equal')
+
+
+
+
+
