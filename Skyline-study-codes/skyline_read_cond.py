a1ff111f108e6ca468672e37c9b335c7960a36c6
Claire Braboszcz <claire@guakamole.org>
2019-10-03 12:35:45 +0100

    removed behaviour data

--- Skyline-study-codes/skyline_read_cond.py
+++ Skyline-study-codes/skyline_read_cond.py
@@ -16,7 +16,7 @@ import pandas as pd
 #-------------- Parameters -----------------
 
 # enter participant ID
-pp_id =647
+pp_id =786
 
 # Enter participant's experimental group, chose from:
 #'Study ID EEG Female'
@@ -24,8 +24,8 @@ pp_id =647
 #'Study ID Main Male'
 #'Study ID Main Female'
 
-pp_group = 'Study ID EEG Female'
-pp_cond = 'Cond EEG Female'
+pp_group = 'Study ID EEG Male'
+pp_cond = 'Cond EEG Male'
 #---------------------------------------------
 
 
