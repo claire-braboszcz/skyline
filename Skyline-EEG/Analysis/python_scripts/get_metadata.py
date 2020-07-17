8e5129cc8dca06980b53712588d1a7a2099995d1
Claire Braboszcz <claire@guakamole.org>
2020-07-17 17:28:51 +0100

    creates csv file with go nogo trials metadata

--- Skyline-EEG/Analysis/python_scripts/get_metadata.py
+++ Skyline-EEG/Analysis/python_scripts/get_metadata.py
@@ -10,32 +10,55 @@ use the events.tsv file to create an epoch metadata file to read with mne python
 @author: claire
 """
 
-from config import (fname, erp_bandpass_fmin, erp_bandpass_fmax, 
-                    events_dict, 
-                    sfreq,
-                    tmin_gng, tmax_gng, event_dict_gng,
-                    screen_offset,
-                    reject_criteria, 
-                    flat_criteria, 
-                    baseline_gng, 
-                    n_jobs)
+from config import (fname, subject_ids, sessions)
 
 import pandas as pd
+import numpy as np
 
 
-subj=146
-sess=1
+for subj in subject_ids:
+       for sess in sessions:
+            
+            print('Processing subject:', subj, 'session:', sess)
 
-logfile=pd.read_csv(fname.logfile(subject='sub-'+ str(subj), 
-                                   session='ses-'+str(sess)), sep='\t')
+           
+            logfile=pd.read_csv(fname.logfile(subject='sub-'+ str(subj), 
+                                               session='ses-'+str(sess)), sep='\t')
+            
+            
+            # find indexes of task start - go no go was done in 2nd and 3rd
+            ind_bloc=logfile[logfile["value"]==55].index.tolist()
+            
+            if len(ind_bloc) == 5:
+            
+                df=logfile.iloc[ind_bloc[1]:ind_bloc[2], :]
+                
+                df=df.append(logfile.iloc[ind_bloc[3]:ind_bloc[4], :])
+            
+            elif len(ind_bloc) == 4:
+                
+                df=logfile.iloc[ind_bloc[0]:ind_bloc[1], :]
+                
+                df=df.append(logfile.iloc[ind_bloc[2]:ind_bloc[3], :])
+            
+            # create column to indicate button press
+            df['response']=np.nan
+            
+            df.loc[df['value']==8, 'response'] = 'button press'
+            df.response=df.response.shift(-1)
+            
+            df.loc[df['value']==11, 'type'] = 'go'
+            df.loc[df['value']==13, 'type'] = 'nogo'
+            
+            df.loc[(df['type']=='go') &( df['response']=='button press'), 'accuracy' ] = 'correct'
+            df.loc[(df['type']=='go') &( df['response']== ''), 'accuracy' ] = 'incorrect'
 
+            df.loc[(df['type']=='nogo') &( df['response']=='button press'), 'accuracy' ] = 'false alarm'
+            
+            # remove rows that are not go or nogo
+            
+            df=df[df['type'].notnull()]
+            
+            df.to_csv(fname.metadata_gng(subject='sub-'+ str(subj), session='ses-'+str(sess)))
 
-# find indexes of task start - go no go was done in 2nd and 3rd
-ind_bloc=logfile[logfile["value"]==55].index.tolist()
-
-
-df=logfile.iloc[ind_bloc[1]:ind_bloc[2], :]
-
-df=df.append(logfile.iloc[ind_bloc[3]:ind_bloc[4], :])
-
-
+            del df, logfile 
